a1cb2b86cb2d2494df7b1238dce99e9509a3de28
ruin276og <yamnahamdi49@gmail.com>
2025-09-25 14:20:36 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,49 +1,133 @@
-/**
- * deploy-commands.js - Registers all slash commands for the community server bot
- */
-
+// deploy-commands.js
 const { REST, Routes } = require('discord.js');
-const config = require('./config.json');
+const { clientId, guildId, token } = require('./config.json');
 
+// All the slash commands your bot will register
 const commands = [
-    // Moderation
-    { name: 'ban', description: 'Ban a member', options: [{ name: 'user', description: 'User to ban', type: 6, required: true }, { name: 'reason', description: 'Reason for ban', type: 3 }] },
-    { name: 'unban', description: 'Unban a user by ID', options: [{ name: 'userid', description: 'ID of the user to unban', type: 3, required: true }] },
-    { name: 'kick', description: 'Kick a member', options: [{ name: 'user', description: 'User to kick', type: 6, required: true }, { name: 'reason', description: 'Reason', type: 3 }] },
-    { name: 'purge', description: 'Delete messages', options: [{ name: 'amount', description: 'Number of messages (1-100)', type: 4, required: true }] },
-    { name: 'warn', description: 'Warn a member', options: [{ name: 'user', description: 'User to warn', type: 6, required: true }, { name: 'reason', description: 'Reason for warning', type: 3 }] },
-    { name: 'mute', description: 'Mute a member', options: [{ name: 'user', description: 'User to mute', type: 6, required: true }, { name: 'duration', description: 'Duration (e.g. 10m, 2h)', type: 3 }, { name: 'reason', description: 'Reason', type: 3 }] },
-    { name: 'unmute', description: 'Unmute a member', options: [{ name: 'user', description: 'User to unmute', type: 6, required: true }] },
-    { name: 'lockdown', description: 'Lock/unlock channel', options: [{ name: 'action', description: 'lock or unlock', type: 3, required: true, choices: [{ name: 'lock', value: 'lock' }, { name: 'unlock', value: 'unlock' }] }] },
-    { name: 'slowmode', description: 'Set channel slowmode in seconds', options: [{ name: 'seconds', description: 'Seconds (0-21600)', type: 4, required: true }] },
-
-    // Utility
-    { name: 'ping', description: 'Check bot latency' },
-    { name: 'help', description: 'List available commands' },
-    { name: 'serverinfo', description: 'Get server information' },
-    { name: 'userinfo', description: 'Get user information', options: [{ name: 'user', description: 'User to lookup', type: 6, required: true }] },
-
-    // System / Announcements
-    { name: 'announce', description: 'Send announcement', options: [{ name: 'channel', description: 'Channel to announce', type: 7, required: true }, { name: 'message', description: 'Message content', type: 3, required: true }, { name: 'embed', description: 'Send as embed?', type: 5 }] },
-    { name: 'dm', description: 'Send DM to a user', options: [{ name: 'user', description: 'User to DM', type: 6, required: true }, { name: 'message', description: 'Message content', type: 3, required: true }] },
-    { name: 'roleadd', description: 'Add role to user', options: [{ name: 'user', description: 'User', type: 6, required: true }, { name: 'role', description: 'Role name or ID', type: 3, required: true }] },
-    { name: 'roleremove', description: 'Remove role from user', options: [{ name: 'user', description: 'User', type: 6, required: true }, { name: 'role', description: 'Role name or ID', type: 3, required: true }] },
-    { name: 'nick', description: 'Change nickname', options: [{ name: 'user', description: 'User', type: 6, required: true }, { name: 'nickname', description: 'New nickname', type: 3, required: true }] }
+  {
+    name: 'ping',
+    description: 'Replies with Pong! (Check bot latency)',
+  },
+  {
+    name: 'hello',
+    description: 'Say hello to the bot!',
+  },
+  {
+    name: 'announce',
+    description: 'Send an announcement to a channel',
+    options: [
+      {
+        type: 7, // CHANNEL
+        name: 'channel',
+        description: 'Channel to send the announcement in',
+        required: true,
+      },
+      {
+        type: 3, // STRING
+        name: 'message',
+        description: 'The announcement message',
+        required: true,
+      },
+    ],
+  },
+  {
+    name: 'clear',
+    description: 'Clear a number of messages from a channel',
+    options: [
+      {
+        type: 4, // INTEGER
+        name: 'amount',
+        description: 'Number of messages to delete (1–100)',
+        required: true,
+      },
+    ],
+  },
+  {
+    name: 'userinfo',
+    description: 'Get information about a user',
+    options: [
+      {
+        type: 6, // USER
+        name: 'target',
+        description: 'The user to get info about',
+        required: true,
+      },
+    ],
+  },
+  {
+    name: 'serverinfo',
+    description: 'Get information about this server',
+  },
+  {
+    name: 'dm',
+    description: 'Send a DM to a user',
+    options: [
+      {
+        type: 6, // USER
+        name: 'user',
+        description: 'The user to DM',
+        required: true,
+      },
+      {
+        type: 3, // STRING
+        name: 'message',
+        description: 'The message to send',
+        required: true,
+      },
+    ],
+  },
+  {
+    name: 'kick',
+    description: 'Kick a member from the server',
+    options: [
+      {
+        type: 6, // USER
+        name: 'target',
+        description: 'The user to kick',
+        required: true,
+      },
+      {
+        type: 3, // STRING
+        name: 'reason',
+        description: 'Reason for the kick',
+        required: false,
+      },
+    ],
+  },
+  {
+    name: 'ban',
+    description: 'Ban a member from the server',
+    options: [
+      {
+        type: 6, // USER
+        name: 'target',
+        description: 'The user to ban',
+        required: true,
+      },
+      {
+        type: 3, // STRING
+        name: 'reason',
+        description: 'Reason for the ban',
+        required: false,
+      },
+    ],
+  },
 ];
 
-const rest = new REST({ version: '10' }).setToken(config.bot.token);
+const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
-    try {
-        console.log('Started refreshing application (/) commands...');
+  try {
+    console.log('Started refreshing application (/) commands.');
 
-        await rest.put(
-            Routes.applicationGuildCommands(config.bot.clientId, config.bot.guildId),
-            { body: commands }
-        );
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
 
-        console.log('Successfully reloaded all commands!');
-    } catch (error) {
-        console.error(error);
-    }
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
 })();
+
